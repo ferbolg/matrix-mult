8411dc48b02d92b7a26dd18f9c00f2431741d8ed
Serhii Zinchuk <ferbolg@gmail.com>
2020-01-19 20:47:48 +0200

    Logging added

--- src/main/java/multparallel/ParallelMatrixMultiplication.java
+++ src/main/java/multparallel/ParallelMatrixMultiplication.java
@@ -1,10 +1,15 @@
 package multparallel;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class ParallelMatrixMultiplication {
+
+    private static Logger logger = LogManager.getLogger(ParallelMatrixMultiplication.class);
 
     private int[][] a;
     private int[][] b;
@@ -26,21 +31,22 @@ public class ParallelMatrixMultiplication {
     }
 
     public void multiplyMatrices() {
-        try {
-            ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE);
 
-            for (int i = 0; i < mSize; i++) {
-                for (int j = 0; j < mSize; j++) {
-                    CellTask ct = new CellTask(i, j, mSize, a, b, c);
-                    executor.execute(ct);
-                }
+        ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE);
+
+        for (int i = 0; i < mSize; i++) {
+            for (int j = 0; j < mSize; j++) {
+                CellTask ct = new CellTask(i, j, mSize, a, b, c);
+                executor.execute(ct);
             }
+        }
 
-            executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.HOURS);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        executor.shutdown();
+        try {
+            executor.awaitTermination(3, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            String errorMessage = "Interrupted exception while awaitTermination: " + e.getMessage();
+            logger.error(errorMessage);
         }
     }
 }
